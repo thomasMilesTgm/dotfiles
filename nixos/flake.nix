9cde1542ecbb5deffa7ee6d32408f2711a95fdd5
Tom Miles <tom@ai-nc.com>
2024-04-15 10:58:26 +1000

    fix: busted

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,7 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    monorepo.url = "path:///home/user/code/monorepo";
 
     # home-manager = {
     #   url = "github:nix-community/home-manager";
@@ -11,7 +10,7 @@
     # };
   };
 
-  outputs = { self, nixpkgs, monorepo, ... }@inputs:
+  outputs = { self, nixpkgs, ... }@inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
